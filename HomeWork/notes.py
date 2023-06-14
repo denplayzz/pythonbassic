be5ffe19ef200026105df359729149ac333d7564
Denys <94934712+denplayzz@users.noreply.github.com>
2023-06-14 19:36:11 +0200

    new_commit

--- HomeWork/notes.py
+++ HomeWork/notes.py
@@ -1,24 +1,19 @@
 # Програма заметки
 # Словарь для записи заметок
-notes_users = {
 
-}
-
-
-# Основная функция для меню
-def menu():
-    while True:
+def menu(notes_users):
+ while True:
         user_question = input(
             'Вы в програме "Заметки", доступные команды: \nДобавить заметку = add\nУдалить заметку = delete \nСортировать заметки = sort\nПосмотреть заметки = show\nНажмите Enter что бы выйти\n> ').lower()
         try:
             if user_question == "add":
-                add_notes()
+                add_notes(notes_users)
             if user_question == "delete":
-                delete_notes()
+                delete_notes(notes_users)
             if user_question == "show":
-                show_notes()
+                show_notes(notes_users)
             if user_question == "sort":
-                menu_sort()
+                menu_sort(notes_users)
             if user_question == "":
                 exit()
 
@@ -26,29 +21,31 @@ def menu():
 
         except Exception:
             pass
+    return notes_users
 
 
 # Функция для меню sort
-def menu_sort():
+def menu_sort(notes_users):
     while True:
         user_question_sort = input(
             "Как вы хотите сортировать заметки?\nОд новой до старой = earlist\nОд старой до новой = latest\nОт самой короткой до длинной = shortest\nОт самой длинной до короткой = longtest\nНажмите Enter что бы выйти из меню сортировки\n> ").lower()
         if user_question_sort == "earlist":
-            show_notes()
+            show_notes(notes_users)
         if user_question_sort == "latest":
-            latest()
+            latest(notes_userss)
         if user_question_sort == "shortest":
-            shortest()
+            shortest(notes_users)
         if user_question_sort == "longtest":
-            longtest()
+            longtest(notes_users)
         if user_question_sort == "":
             break
         else:
             pass
+    return notes_users
 
 
 # Функция с "menu", для добавления заметок
-def add_notes():
+def add_notes(notes_users):
     i = 1
     while True:
         y = input('Введите заметку: ')
@@ -60,11 +57,12 @@ def add_notes():
             continue
         else:
             break
+    return notes_users
 
 
 # Функция с "menu", для удаления заметок
-def delete_notes():
-    show_notes()
+def delete_notes(notes_users):
+    show_notes(notes_users)
     notes_delete = input("Введите номер заметки чтобы удалить её: ")
     notes_delete = int(notes_delete)
     if notes_delete in notes_users:
@@ -75,7 +73,7 @@ def delete_notes():
 
 
 # Функция с "menu_sort", для сортировки заметок, выводит сохраненные заметки в хронологическом порядке - от позднейшей до самой ранней
-def latest():
+def latest(notes_users):
     x = dict(reversed(notes_users.items()))
     for num, note in x.items():
         print(f"{num}. {note}")
@@ -83,21 +81,21 @@ def latest():
 
 # Функция с "menu_sort", для сортировки заметок, выводит сохраненные заметки в порядке от самой короткой до самой длинной
 
-def shortest():
+def shortest(notes_users):
     sort_long_notes = sorted(notes_users.keys(), key=lambda x: len(notes_users[x]), reverse=False)
     for num in sort_long_notes:
         print(f'{num}: {notes_users[num]}')
 
 
 # Функция с "menu_sort", для сортировки заметок, выводит сохраненные заметки в порядке от самой длинной до самой короткой
-def longtest():
+def longtest(notes_users):
     sorted_notes = sorted(notes_users.keys(), key=lambda x: len(notes_users[x]), reverse=True)
     for num in sorted_notes:
         print(f'{num}: {notes_users[num]}')
 
 
 # Функция с "menu" для показа заметок, так же служит случаем сортировки earlist, от самой ранней до самой поздней
-def show_notes():
+def show_notes(notes_users):
     for num, note in notes_users.items():
         print(f"{num}. {note}")
 
